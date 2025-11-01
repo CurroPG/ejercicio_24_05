cbed64cb53f325a7a3fc47c5243b8de80de265e5
Curro Portillo <211curroportillo@gmail.com>
2025-11-01 19:11:26 +0100

    intento 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la altura: "));
+        String relleno = System.console().readLine("Introduzca el carácter de relleno: ");
+        String espacios = " ";
+        for(int i = 0; i<altura; i++){
+            System.out.print(relleno);
+             
+        }
+        
+        
     }
 }
