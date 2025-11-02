43d3d9fc4c98cf0a065448bf03f041d8702127d0
Curro Portillo <211curroportillo@gmail.com>
2025-11-02 23:15:16 +0100

    ejercicio 24

--- src/App.java
+++ src/App.java
@@ -1,21 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la altura: "));
-        //String relleno = System.console().readLine("Introduzca el carácter de relleno: ");
         int longitudFila = 1;
-        int planta = 1;
         int espacios = altura-1;
 
-        while(planta <= altura){
+        while(longitudFila <= altura){
             for(int i = 0; i<espacios; i++){
                 System.out.print(" ");                
             }
-            for(int i = 1; i<=longitudFila; i++){
+            for(int i = 1; i<longitudFila; i++){
+                System.out.print(i);
+            }
+            for (int i = longitudFila; i > 0; i--) {
                 System.out.print(i);
             }
             System.out.println();
-            planta++;
-            longitudFila +=2;
+            longitudFila ++;
             espacios--;
         }     
     }
